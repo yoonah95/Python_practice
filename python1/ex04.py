3a8a972e4d92645b4103ac5a4d50c8909682e645
yoonah95 <yoon.a1@hanmail.net>
2020-04-16 16:07:56 +0900

    20200416

---
+++ python1/ex04.py
@@ -0,0 +1,6 @@
+
+for x in range(2,4):
+	for y in range(2,10):
+		print(x,"*",y,"=",x*y)
+		
+	print("\n")
---
+++ python1/ex05.py
@@ -0,0 +1,23 @@
+
+count = 1
+while count < 11 :
+	print(count)
+	count = count + 1
+
+
+
+sum = 0
+a = 0
+while a < 10 :
+	a = a+1
+	sum = sum+a
+print(sum)
+
+a = 0
+while a <10 :
+	a=a+1
+	if a<3: continue
+	if a>10: break
+else:
+	print("else block")
+print("done")
