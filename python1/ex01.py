d1d614c0a83c9a4ca7e0e43aa0fd75c66efdcba7
yoonah95 <yoon.a1@hanmail.net>
2020-04-16 15:31:00 +0900

    today

---
+++ python1/ex01.py
@@ -0,0 +1,12 @@
+sum = 0
+for x in range(1,11) :
+	sum = sum + x
+
+print(sum)
+
+prod = 1
+for x in range(1,11) :
+	prod = prod *x
+
+print(prod)
+
---
+++ python1/ex02.py
@@ -0,0 +1,13 @@
+
+for x in range(10) :
+	if x > 3 : break
+	print(x)
+print('done')
+
+
+for x in range(10) :
+	if x<8 :continue
+	print(x)
+print('done')
+
+
